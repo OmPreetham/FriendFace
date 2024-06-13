a5c32da9340be4b6904f4d6c7dddbf0dec5fa060
Om Preetham <ompreethambandi@gmail.com>
2024-06-13 15:05:23 -0500

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Om Preetham Bandi on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
